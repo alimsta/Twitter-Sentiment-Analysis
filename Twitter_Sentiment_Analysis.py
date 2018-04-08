c7957131cc94892bc05e70944e873ccdf62fed51
Alim Momin <alimsta@users.noreply.github.com>
2018-04-07 21:32:08 -0400

    Update Twitter_Sentiment_Analysis.py

--- Twitter_Sentiment_Analysis.py
+++ Twitter_Sentiment_Analysis.py
@@ -2,10 +2,10 @@ from textblob import TextBlob
 import tweepy
 
 #Authenticate application with Twitter
-consumer_key = 'PGhXmMzNrGRxs208lFvapf9Sq'
-consumer_secret = 'gR1vL5EdqzttSXduIqjGfIdsCmwsOp8IoMqvwd9vi9YHBeNRUy'
-access_token = '329423443-Z0bXFdMEc43FEqSHJ9NsBtKnJx1JL3OC3JC8Y4Qa'
-access_secret = 'fzccrr8C67M8LzYYmqWu8Clredvdj4aM6qPk3X8BuFfEL'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
 
 authentication = tweepy.OAuthHandler(consumer_key, consumer_secret)
 authentication.set_access_token(access_token, access_secret)
